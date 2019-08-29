77822956cca557dd694bf54f808986da6d7d54d7
MackenzieDoyle <mackenziedoyle@huskers.unl.edu>
2019-08-29 11:18:08 -0500

    Added Major
    Added a line that prints my major

---
+++ hello.c
@@ -0,0 +1,15 @@
+
+/**
+  * Author: Mackenzie Doyle
+  * Date: Auguest 28th, 2019
+  *
+  *This program is a simple program to test out atom. Prints "Hello World!"
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Mackenzie Doyle\n");
+  printf("Computer Science and Math\n");
+  return 0;
+}
